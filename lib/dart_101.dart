560c3f110de731e8fb3f456974e46d96122e03b8
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-01-11 23:51:42 +0300

    first commit

---
+++ lib/dart_101.dart
@@ -0,0 +1,60 @@
+import 'hardware_andro.dart';
+
+void main() {
+  //var val1 = "string1";
+  // String val2 = "string,asdasd";
+
+  // int _int1 = 1;
+//  var int1 = 6;
+//  int? x;
+
+  //bool _bool = true;
+  // bool bool_ = false;
+  //bool? xBool;
+
+  // print(val1);
+  //print(_int1);
+  // print(x);
+
+  // print(xBool);
+
+  // List<int> _list = [1, 2, 3, 4];
+  // var list = ["a", 1];
+
+  //print("${list} ${int1}");
+
+  //var _split = val2.split(",");
+  // var _toString = int1.toString();
+  // if (_int1 > 0) {
+  //   print("okey");
+  // } else {
+  //  print("false");
+  var hwa1 = new HardwareAndro("videoTitle", "author", 16);
+
+  print(hwa1.videoTitle);
+}
+
+  //bool isOkey = _list.length > 3 ? true : false;
+  //print(isOkey);
+
+ // for (var element in _list) {
+//    print(element.toString());
+//  }
+ // for (var i = 0; i < int1; i++) {
+//    print(i);
+//  }
+  //var total = _sum(5, 32);
+  // print(total);
+  //**final use runtime & immutable veriable
+  //final _final1 = DateTime.now();
+  //**const use commpiler time & immutable
+  //const _const = DateTime.now();
+
+
+
+//void sum(int val1, int val2) {
+ // print("result: ${(val1 + val2)}");
+
+
+//int _sum(int val1, int val2) {
+  //return val1 + val2;
---
+++ lib/hardware_andro.dart
@@ -0,0 +1,11 @@
+class HardwareAndro {
+  String? videoTitle;
+  var author;
+  int? number;
+
+  HardwareAndro(String videoTitle, var author, int number) {
+    this.videoTitle = videoTitle;
+    this.author = author;
+    this.number = number;
+  }
+}
