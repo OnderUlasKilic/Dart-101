21713a2b870ed805b0a88e5e637de71c4603a68f
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-01-12 23:23:20 +0300

    Flutter-Dart 101

--- bin/dart_101.dart
+++ bin/dart_101.dart
@@ -1,65 +1,69 @@
 import 'package:dart_101/hardware_andro.dart';
 
 void main() {
-  var val1 = "string1";
-  String val2 = "string,asdasd";
+//   var val1 = "string1";
+//   String val2 = "string,asdasd";
 
-  int _int1 = 1;
-  var int1 = 6;
-  int? x;
+//   int _int1 = 1;
+//   var int1 = 6;
+//   int? x;
 
-  bool _bool = true;
-  bool bool_ = false;
-  bool? xBool;
+//   bool _bool = true;
+//   bool bool_ = false;
+//   bool? xBool;
 
-  print(val1);
-  print(_int1);
-  print(x);
+//   print(val1);
+//   print(_int1);
+//   print(x);
 
-  print(xBool);
+//   print(xBool);
 
-  List<int> _list = [1, 2, 3, 4];
-  var list = ["a", 1];
+  // List<int> _list = [1, 2, 3, 4];
+  // var list = ["a", 1];
 
-  print("${list} ${int1}");
+  // print("${list} ${int1}");
 
-  var _split = val2.split(",");
-  var _toString = int1.toString();
-  if (_int1 > 0) {
-    print("okey");
-  } else {
-    print("false");
-    var hwa1 = new HardwareAndro("Ahmet", "author", 16);
+  // var _split = val2.split(",");
+  // var _toString = int1.toString();
+  // if (_int1 > 0) {
+  //   print("okey");
+  // } else {
+  //   print("false");
+  var hwa1 = new HardwareAndro("Ahmet", "author", 16);
 
-    print(hwa1.videoTitle);
-    print(hwa1.author);
-    print(hwa1.number);
-  }
+  var hwa2 = new HardwareAndro.myCustructor("aeraaa");
 
-  bool isOkey = _list.length > 3 ? true : false;
-  print(isOkey);
+  print(hwa1.videoTitle);
+  print(hwa1.author);
+  print(hwa1.number);
 
-  for (var element in _list) {
-    print(element.toString());
-  }
+  print(hwa2.videoTitle);
+}
 
-  for (var i = 0; i < int1; i++) {
-    print(i);
-  }
+  // bool isOkey = _list.length > 3 ? true : false;
+  // print(isOkey);
+
+  // for (var element in _list) {
+  //   print(element.toString());
+  // }
+
+  // for (var i = 0; i < int1; i++) {
+  //   print(i);
+  // }
 
   // final use runtime & immutable veriable;
-  final _final1 = DateTime.now();
+  // final _final1 = DateTime.now();
   // const use commpiler time & immutable;
   // const _const = DateTime.now();
 
-  void sum(int val1, int val2) {
-    print("result: ${(val1 + val2)}");
-  }
+  // void sum(int val1, int val2) {
+  //   print("result: ${(val1 + val2)}");
+  // }
 
-  int _sum(int val1, int val2) {
-    return val1 + val2;
-  }
+  // int _sum(int val1, int val2) {
+  //   return val1 + val2;
+  // }
 
-  var total = _sum(5, 32);
-  print(total);
-}
+  // var total = _sum(5, 32);
+  // print(total);
+// }
--- lib/hardware_andro.dart
+++ lib/hardware_andro.dart
@@ -2,10 +2,33 @@ class HardwareAndro {
   String? videoTitle;
   var author;
   int? number;
+  int? _channel;
+  var _data;
 
-  HardwareAndro(String videoTitle, var author, int number) {
-    this.videoTitle = videoTitle;
-    this.author = author;
-    this.number = number;
+  //Encapsulation
+
+  void set channel(int value) {
+    this._channel = value;
+  }
+
+  int? get __channel {
+    return this._channel;
+  }
+
+// Arrow function Encapsulation
+  set data(var value) => this._data = value;
+  get data => this._data;
+
+  // HardwareAndro(String videoTitle, var author, int number) {
+  //   this.videoTitle = videoTitle;
+  //   this.author = author;
+  //   this.number = number;
+  // }
+  HardwareAndro(this.videoTitle, this.author, this.number) {}
+
+//Custom constructor
+
+  HardwareAndro.myCustructor(String videoTitle) {
+    this.videoTitle = videoTitle.length.toString();
   }
 }
